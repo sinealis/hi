66ea0628780e280012275ab1e5837ef0a7660583
sinealis <judita.csibova@gmail.com>
2021-06-02 17:21:42 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-	if(!showMenu) {
+function toggleMenu () {
+	if (!showMenu) {
 		menuBtn.classList.add("close");
 		menu.classList.add("show");
 		menuNav.classList.add("show");
